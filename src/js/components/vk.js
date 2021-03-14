c76c1abe463861b63e7be375058635f359953c8b
Denis <powerslime@ya.ru>
2021-03-15 01:49:13 +0200

    Fix bug when in UA VK is blocked

--- src/js/components/vk.js
+++ src/js/components/vk.js
@@ -1,8 +1,16 @@
 export default function vk() {
-    VK.Widgets.Group("vk-news", {mode: 4, width: "auto", height: "600"}, 150619577);
-    VK.Widgets.CommunityMessages("vk_community_messages", 150619577, {
-        expandTimeout: "30000",
-        disableNewMessagesSound: "1",
-        tooltipButtonText: "Есть вопрос?"
-    });
+    try {
+        VK.Widgets.Group("vk-news", {mode: 4, width: "auto", height: "600"}, 150619577);
+        VK.Widgets.CommunityMessages("vk_community_messages", 150619577, {
+            expandTimeout: "30000",
+            disableNewMessagesSound: "1",
+            tooltipButtonText: "Есть вопрос?"
+        });
+    } catch (e) {
+        const elem = document.getElementById("vk-news");
+        if (elem) {
+            elem.innerHTML = "<a href='https://vk.com/magicofgods' target='_blank'>https://vk.com/magicofgods</a>"
+        }
+        console.warn("Слава Украине! ВК потiк");
+    }
 }
